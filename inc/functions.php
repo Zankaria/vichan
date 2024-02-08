e6893d776e89f3c6cbf78cc6710ec6112f5abd8f
Zankaria <zankaria.auxa@skiff.com>
2024-02-08 19:59:11 +0100

    functions.php: split off shell_exec_error function

--- inc/functions.php
+++ inc/functions.php
@@ -13,6 +13,7 @@ require_once('inc/bootstrap.php');
 require_once('inc/functions/generation-strategy.php');
 require_once('inc/functions/encoding.php');
 require_once('inc/functions/math.php');
+require_once('inc/functions/interop.php');
 
 
 $microtime_start = microtime(true);
@@ -2766,30 +2767,6 @@ function DNS($host) {
 	}
 
 	return $ip_addr;
-}
-
-function shell_exec_error($command, $suppress_stdout = false) {
-	global $config, $debug;
-
-	if ($config['debug']) {
-		$start = microtime(true);
-	}
-
-	$return = trim(shell_exec('PATH="' . escapeshellcmd($config['shell_path']) . ':$PATH";' .
-		$command . ' 2>&1 ' . ($suppress_stdout ? '> /dev/null ' : '') . '&& echo "TB_SUCCESS"'));
-	$return = preg_replace('/TB_SUCCESS$/', '', $return);
-
-	if ($config['debug']) {
-		$time = microtime(true) - $start;
-		$debug['exec'][] = array(
-			'command' => $command,
-			'time' => '~' . round($time * 1000, 2) . 'ms',
-			'response' => $return ? $return : null
-		);
-		$debug['time']['exec'] += $time;
-	}
-
-	return $return === 'TB_SUCCESS' ? false : $return;
 }
 
 /**
