c5cf32cc5caf4ba0c91dd2fad4a8dae413ca03c7
Zankaria <zankaria.auxa@mailu.io>
2024-04-19 00:18:39 +0200

    cache-driver: use mockery

--- inc/driver/cache-driver.php
+++ inc/driver/cache-driver.php
@@ -1,217 +1,34 @@
 <?php
 
-use RuntimeException;
-
 defined('TINYBOARD') or exit;
 
 /**
  * PHP has no nested or private classes support.
  */
 class CacheDrivers {
-	public static function memcached(string $prefix, string $memcached_server) {
-		$memcached = new Memcached();
-		$memcached->setOption(Memcached::OPT_BINARY_PROTOCOL, true);
-		$memcached->setOption(Memcached::OPT_PREFIX_KEY, $prefix);
-		$memcached->addServers($memcached_server);
-
-		return new class($memcached) implements CacheDriver {
-			private Memcached $inner;
-
-			public function __construct(Memcached $inner) {
-				$this->inner = $inner;
-			}
-
+	public static function mockery(): CacheDriver {
+		/**
+		 * A proxy to the older cache class implementation.
+		 */
+		return new class implements CacheDriver {
 			public function get(string $key): mixed {
-				$ret = $this->inner->get($key);
-				// If the returned value is false but the retrival was a success, then the value stored was a boolean false.
-				if ($ret === false && $this->inner->getResultCode() !== Memcached::RES_SUCCESS) {
-					return null;
-				}
-				return $ret;
-			}
-
-			public function set(string $key, mixed $value, int|false $expires = false): void {
-				$this->inner->set($key, $value, (int)$expires);
-			}
-
-			public function delete(string $key): void {
-				$this->inner->delete($key);
-			}
-
-			public function flush(): void {
-				$this->inner->flush();
-			}
-		};
-	}
-
-	public static function redis(string $prefix, string $host, int $port, string $password, string $database) {
-		$redis = new Redis();
-		$redis->connect($host, $port);
-		if ($password) {
-			$redis->auth($password);
-		}
-		if (!$redis->select($database)) {
-			return false;
-		}
-
-		return new class($prefix, $redis) implements CacheDriver {
-			private string $prefix;
-			private Redis $inner;
-
-			public function __construct(string $prefix, Redis $inner) {
-				$$this->prefix = $prefix;
-				$this->inner = $inner;
-			}
-
-			public function get(string $key): mixed {
-				$ret = $this->inner->get($this->prefix . $key);
+				$ret = \Cache::get($key);
 				if ($ret === false) {
 					return null;
 				}
-				return json_decode($ret, true);
-			}
-
-			public function set(string $key, mixed $value, int|false $expires = false): void {
-				if ($expires === false) {
-					$this->inner->set($this->prefix . $key, json_encode($value));
-				} else {
-					$this->inner->setex($this->prefix . $key, $expires, json_encode($value));
-				}
-			}
-
-			public function delete(string $key): void {
-				$this->inner->del($this->prefix . $key);
-			}
-
-			public function flush(): void {
-				$this->inner->flushDB();
-			}
-		};
-	}
-
-	public static function apcu() {
-		return new class implements CacheDriver {
-			public function get(string $key): mixed {
-				$success = false;
-				$ret = apcu_fetch($key, $success);
-				if ($success === false) {
-					return null;
-				}
 				return $ret;
 			}
 
-			public function set(string $key, mixed $value, int|false $expires = false): void {
-				apcu_store($key, $value, (int)$expires);
+			public function set(string $key, mixed $value, mixed $expires = false): void {
+				\Cache::set($key, $value, $expires);
 			}
 
 			public function delete(string $key): void {
-				apcu_delete($key);
+				\Cache::delete($key);
 			}
 
 			public function flush(): void {
-				apcu_clear_cache();
-			}
-		};
-	}
-
-	public static function filesystem(string $prefix, string $base_path) {
-		if ($base_path[strlen($base_path) - 1] !== '/') {
-			$base_path = "$base_path/";
-		}
-
-		if (!is_dir($base_path) || is_writable($base_path)) {
-			return false;
-		}
-
-		return new class($prefix, $base_path) implements CacheDriver {
-			private string $prefix;
-			private string $base_path;
-
-			public function __construct(string $prefix, string $base_path) {
-				$this->prefix = $prefix;
-				$this->base_path = $base_path;
-			}
-
-			public function get(string $key): mixed {
-				$key = str_replace('/', '::', $key);
-				$key = str_replace("\0", '', $key);
-				$key = $this->prefix . $key;
-
-				if (!file_exists("{$this->base_path}/{$key}")) {
-					return null;
-				}
-
-				$data = file_get_contents('tmp/cache/'.$key);
-				return json_decode($data, true);
-			}
-
-			public function set(string $key, mixed $value, int|false $expires = false): void {
-				$key = str_replace('/', '::', $key);
-				$key = str_replace("\0", '', $key);
-				$key = $this->prefix . $key;
-
-				$data = json_encode($value);
-				file_put_contents("{$this->base_path}/{$key}", $data);
-			}
-
-			public function delete(string $key): void {
-				$key = str_replace('/', '::', $key);
-				$key = str_replace("\0", '', $key);
-				$key = $this->prefix . $key;
-
-				@unlink("{$this->base_path}/{$key}");
-			}
-
-			public function flush(): void {
-				$files = glob("{$this->base_path}/{$this->prefix}*");
-				foreach ($files as $file) {
-					@unlink($file);
-				}
-			}
-		};
-	}
-
-	public static function php_array() {
-		return new class implements CacheDriver {
-			private static array $inner = [];
-
-			public function get(string $key): mixed {
-				return isset(self::$inner[$key]) ? self::$inner[$key] : null;
-			}
-
-			public function set(string $key, mixed $value, int|false $expires = false): void {
-				self::$inner[$key] = $value;
-			}
-
-			public function delete(string $key): void {
-				unset(self::$inner[$key]);
-			}
-
-			public function flush(): void {
-				self::$inner = array();
-			}
-		};
-	}
-
-	/**
-	 * No-op cache. Useful for testing.
-	 */
-	public static function none() {
-		return new class implements CacheDriver {
-			public function get(string $key): mixed {
-				return null;
-			}
-
-			public function set(string $key, mixed $value, int|false $expires = false): void {
-				// No-op.
-			}
-
-			public function delete(string $key): void {
-				// No-op.
-			}
-
-			public function flush(): void {
-				// No-op.
+				\Cache::flush();
 			}
 		};
 	}
